ee0455e1e32676b54a72df274ee987a991804299
Zili Feng <fengzee@fengzee.me>
2025-02-19 10:00:00 +0800

    Implement a plugin to copy previous daily note on workspace load. Remove sample features.

--- main.ts
+++ main.ts
@@ -1,85 +1,67 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { moment } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface DailyNotePluginSettings {
+	enabled: boolean;
+	dailyNotesFolder: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DailyNotePluginSettings = {
+	enabled: true,
+	dailyNotesFolder: '每日笔记'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DailyNotePlugin extends Plugin {
+	settings: DailyNotePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Copy Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new DailyNoteSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// 注册加载事件
+		this.app.workspace.onLayoutReady(() => this.onWorkspaceReady());
 	}
 
 	onunload() {
 
+	}
+
+	async onWorkspaceReady() {
+		if (!this.settings.enabled) return;
+
+		const folder = this.app.vault.getAbstractFileByPath(this.settings.dailyNotesFolder);
+		if (!folder) return;
+
+		const today = moment().format('YYYY-MM-DD');
+		const todayPath = `${this.settings.dailyNotesFolder}/${today}.md`;
+
+		// 检查今日笔记是否存在
+		const todayNote = this.app.vault.getAbstractFileByPath(todayPath);
+		if (todayNote) return;
+
+		// 查找最近的一篇日记
+		const files = await this.app.vault.getAllLoadedFiles();
+		const dailyNotes = files
+			.filter(file => 
+				file instanceof TFile &&
+				file.path.startsWith(this.settings.dailyNotesFolder + '/') &&
+				file.path.endsWith('.md') &&
+				file.path < todayPath
+			)
+			.sort((a, b) => b.path.localeCompare(a.path));
+
+		if (dailyNotes.length > 0) {
+			const lastNote = dailyNotes[0] as TFile;
+			const content = await this.app.vault.read(lastNote);
+			await this.app.vault.create(todayPath, content);
+		} else {
+			// 如果没有之前的笔记，创建空笔记
+			await this.app.vault.create(todayPath, '');
+		}
 	}
 
 	async loadSettings() {
@@ -107,27 +89,36 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class DailyNoteSettingTab extends PluginSettingTab {
+	plugin: DailyNotePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DailyNotePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('启用自动创建每日笔记')
+			.setDesc('工作区加载时，自动创建当天的笔记')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enabled)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.enabled = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('每日笔记目录')
+			.setDesc('存放每日笔记的目录名称')
+			.addText(text => text
+				.setPlaceholder('每日笔记')
+				.setValue(this.plugin.settings.dailyNotesFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.dailyNotesFolder = value;
 					await this.plugin.saveSettings();
 				}));
 	}
